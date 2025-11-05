751073777ddfffff4ced6b4b58b249381c080a49
Cauã <cauafreirw@gmail.com>
2025-11-05 15:37:13 -0300

    Criando a logica da model

---
+++ Projeto1AspNet/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto1AspNet.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal quantidade { get; set; }
+    }
+}
--- Projeto1AspNet/Repositorio/ProdutoRepositorio.cs
+++ Projeto1AspNet/Repositorio/ProdutoRepositorio.cs
@@ -1,4 +1,7 @@
-﻿namespace Projeto1AspNet.Repositorio
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Projeto1AspNet.Repositorio
 {
     public class ProdutoRepositorio
     {
